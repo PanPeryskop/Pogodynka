db0cb18bd8d9242614a82482b4a7ccc9a6cf6acd
PanPeryskop <stanislaw.gadek@o365.us.edu.pl>
2024-12-22 15:52:09 +0100

    Set base path in Vite config to match repository name

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Pogodynka/',  // Add this line - should match your repository name
 })
