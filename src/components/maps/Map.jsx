7711ede4195f8024772fbd39dfac2147e8ec8d75
PanPeryskop <stanislaw.gadek@o365.us.edu.pl>
2024-12-23 22:53:31 +0100

    add custom marker icon for map component (work on linux)

--- src/components/maps/Map.jsx
+++ src/components/maps/Map.jsx
@@ -9,6 +9,22 @@ import {
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "./Map.css";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+import L from "leaflet";
+import { IoMdPin } from "react-icons/io";
+
+const customIcon = L.divIcon({
+  className: 'custom-pin-icon',
+  html: `<div style="color: #e74c3c; font-size: 24px;"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 384 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path></svg></div>`,
+  iconSize: [24, 24],
+  iconAnchor: [12, 24],
+  popupAnchor: [0, -24]
+});
+
+L.Marker.prototype.options.icon = customIcon;
+
 
 const MapEvents = ({ onLocationSelect }) => {
   useMapEvents({
